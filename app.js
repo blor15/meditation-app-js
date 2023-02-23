fe6cb6a6734f9ffa3563dbb08a3d44b19a4065b6
Brandon <blor15@hotmail.com>
2023-02-22 18:41:58 -0700

    Sound plays when the play button is pressed

--- app.js
+++ app.js
@@ -14,6 +14,27 @@ const app = () => {
 
   //Duration
   let fakeDuration = 600;
+
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  //sound
+  play.addEventListener("click", () => {
+    checkPlaying(song);
+  });
+
+  //A function to pause and play the sounds
+  const checkPlaying = (song) => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src = "./svg/pause.svg";
+    } else {
+      song.pause();
+      video.pause();
+      play.src = "./svg/play.svg";
+    }
+  };
 };
 
 app();
